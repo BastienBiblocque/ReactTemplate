6ee76a3f42de39154505f56e29cfe2c01ddd47c2
Bastien <45488913+BastienBiblocque@users.noreply.github.com>
2023-01-02 11:52:09 +0100

    installation du router et de tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Index from "./Page";
 
-function App() {
+export default function BasicExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Index />} />
+          </Routes>
+      </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/Page/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Index() {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    );
+}
+
+export default Index;
