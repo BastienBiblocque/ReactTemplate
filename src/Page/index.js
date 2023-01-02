c7bf0d4cdb815e76bca7f8db4e191f7610bb4117
LyneF <Lyne.Friederich@hotmail.com>
2023-01-02 16:41:06 +0100

    Ajout de test dans la page Index.

--- src/Page/index.js
+++ src/Page/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 function Index() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            <p>Coucou test 1 2</p>
+        </>
     );
 }
 
